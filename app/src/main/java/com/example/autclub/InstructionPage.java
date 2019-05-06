2425fe29a292896f54f5432ab4e0eddf59ddcdf2
Ankita <ankitapanwar428@gmail.com>
2019-05-06 16:20:05 +1200

    changes in gradle app

--- app/src/main/java/com/example/autclub/InstructionPage.java
+++ app/src/main/java/com/example/autclub/InstructionPage.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+
 
 import com.github.paolorotolo.appintro.AppIntro;
 
